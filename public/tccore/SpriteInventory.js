508cf09bd8f8f2a6d1950589e584ee7523538b94
Joseph Mordetsky <jmordetsky@01734a-jmordetsky.corp.zynga.com>
2012-08-01 13:13:37 -0400

    added a var on my branch

--- public/tccore/SpriteInventory.js
+++ public/tccore/SpriteInventory.js
@@ -32,6 +32,7 @@ var SpriteInventory = (function(){
 	var defaultParts = ["HD", "LA", "LG", "LH","RA", "RH", "SH", "TR"];
 	var directions = ["North", "NorthEast", "East", "SouthEast", "South", "SouthWest", "West", "NorthWest"];
 	var directionNum = [-1, -2, -3, -4, -5, -6, -7, -8];
+	var aMap = "";
 	var inventory = {};
 
 	//altar floor cells
